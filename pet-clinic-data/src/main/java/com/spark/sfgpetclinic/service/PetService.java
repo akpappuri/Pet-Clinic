6844a25deadba40f5d3c67e435e47234539393f3
Anand Pappuri <apappuri@gmail.com>
2021-11-07 06:49:42 -0500

    Added service for Owner, Pet and Vet.

---
+++ pet-clinic-data/src/main/java/com/spark/sfgpetclinic/service/PetService.java
@@ -0,0 +1,12 @@
+package com.spark.sfgpetclinic.service;
+
+import com.spark.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/spark/sfgpetclinic/service/VetService.java
@@ -0,0 +1,12 @@
+package com.spark.sfgpetclinic.service;
+
+import com.spark.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
