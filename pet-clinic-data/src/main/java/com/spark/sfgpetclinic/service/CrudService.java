cb4d5a7a1af17210733bd5442bd6117997c4eafd
Anand Pappuri <apappuri@gmail.com>
2021-11-08 22:33:11 -0500

    refactoring to common crud service

---
+++ pet-clinic-data/src/main/java/com/spark/sfgpetclinic/service/CrudService.java
@@ -0,0 +1,12 @@
+package com.spark.sfgpetclinic.service;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+    T findByID(ID id);
+    T save(T object);
+
+    void delete(T object);
+    void deleteById(ID id);
+}
